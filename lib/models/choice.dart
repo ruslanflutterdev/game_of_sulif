e775dd1462643236bca04bf8de1f091435ec8900
Ruslan Tyutyunnikov <Rusrus1984_8@mail.ru>
2025-03-18 19:54:49 +0500

    Чистим main.dart, создаем модель выбора возможных вариантов, подключаем картинки

---
+++ lib/models/choice.dart
@@ -0,0 +1,13 @@
+class Choice {
+  final String name;
+  final String imagePath;
+
+  Choice({required this.name, required this.imagePath});
+
+}
+
+final List<Choice> choices = [
+  Choice(name: 'Камень', imagePath: 'assets/rock.png'),
+  Choice(name: 'Ножницы', imagePath: 'assets/scissors.png'),
+  Choice(name: 'Бумага', imagePath: 'assets/paper.png'),
+];
